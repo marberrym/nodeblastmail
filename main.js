94e29949eb170714a929efb9290a9519fc4ebf36
Matthew <marberrym@gmail.com>
2019-01-09 17:50:07 -0500

    Well thats a start...

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 //email logic.
+const nodemailer = require('nodemailer');
+const csv = require('csvtojson/v1');
+const schedule = require('node-schedule');
+const template = require('./template.js');
+require('dotenv').config();
+
+console.log(process.env.account);
+console.log(process.env.pw);
+
+const account = {
+    user: process.env.account,
+    pass: process.env.pw
+}
+
+var mailBlaster = nodemailer.createTransport({
+   pool: true,
+   host: 'smtp.gmail.com',
+   port: 465,
+   secure: true,
+   auth: account 
+}) 
